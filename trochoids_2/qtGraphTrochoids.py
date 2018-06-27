2c4be3e342923fb9737c6d0f3c6fedca8902d0a8
L H <pulse@tuta.io>
2018-06-27 15:10:37 -0400

    trying to add graphs

--- trochoids_2/qtGraphTrochoids.py
+++ trochoids_2/qtGraphTrochoids.py
@@ -3,8 +3,8 @@ import numpy as np
 from math import cos,sin
 from time import sleep
 
-win = pg.GraphicsWindow()
-win.setWindowTitle('Trochoids')
+#win = pg.GraphicsWindow()
+#win.setWindowTitle('Trochoids')
 
 bigR = 125
 smallR = 75
@@ -15,13 +15,22 @@ angle = 0
 theta = 0.2
 steps = int(6*3.14/theta)
 
+dataX = np.empty(steps)
+dataY = np.empty(steps)
+dataArray = np.array([dataX, dataY], np.float)
+
 for t in range(0,steps):
     angle+=theta
 
     x = (bigR - smallR) * cos(angle) + pointR * cos(((bigR-smallR)/smallR)*angle)
     y = (bigR - smallR) * sin(angle) - pointR * sin(((bigR-smallR)/smallR)*angle)
 
+    dataX[t] = x
+    dataY[t] = y
+
     print("x: ", x)
     print("y: ", y)
 
-    sleep (0.5)
+#p1 = win.addPlot()
+pg.show(dataArray)
+#
