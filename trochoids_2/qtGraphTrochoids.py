e555b179e5cca8a45b1b995dc6ec1dedb8c2aec3
dftbat2sp <pulse@tuta.io>
2018-08-20 20:54:07 -0400

    added comments and trying to refactor so small circle can be watched drawing the shape. Right now only large cirlce is drawing, think I need to look at "scrollingPLot.py" and the update functions with the timer. look at EOF

--- trochoids_2/qtGraphTrochoids.py
+++ trochoids_2/qtGraphTrochoids.py
@@ -4,52 +4,80 @@ import numpy as np
 from math import cos,sin
 from time import sleep 
 
-win = pg.GraphicsWindow()
-win.setWindowTitle('Trochoids')
-win.resize(1000,1000)
 
-bigR = 222
-smallR = 123
-pointR = 90
+win = pg.GraphicsWindow()           # create graphics window
+win.setWindowTitle('Trochoids')     # set title
+win.resize(1000,1000)               # set window size
 
-angle = 0
+# Circle sizes
+statRad = 144               # Stationary Circle size
+rotRad = 72                 # moving circle size
+drawPointRad = rotRad/2     # location from center of moving circle to draw
 
-theta = 0.05
-steps = int(1000 * 3.14 / theta)
+# Drawing paarameters
+startAngle = 0                       # starting angle
+theta = 0.05                    # distance in radians for each step
+oneRotation = int(3.14/theta)        # number of steps for 1 full rotation
+# should only need to do number of rotation less than the ratius of the stationary circle
+steps = int(statRad * oneRotation) 
 
-axisRange = 200
+axisRange = statRad + 50   # Axis label range
 
-dataX = np.empty(steps)
-dataY = np.empty(steps)
+# Stationary Circle
+statRadX = np.empty(steps) # array for x points
+statRadY = np.empty(steps) # corresponding y points
 
-circleX = np.empty(steps)
-circleY = np.empty(steps)
+# moving circle
+rotRadX = np.empty(steps) # array for x points
+rotRadY = np.empty(steps) # corresponding y points
 
-for t in range(0,steps):
-    angle+=theta
-
-    x = (bigR - smallR) * cos(angle) + pointR * cos(((bigR - smallR) / smallR) * angle)
-    y = (bigR - smallR) * sin(angle) - pointR * sin(((bigR - smallR) / smallR) * angle)
-
-    dataX[t] = x
-    dataY[t] = y
-
-    x = bigR * cos(angle)
-    y = bigR * sin(angle)
-
-    circleX[t] = x
-    circleY[t] = y
-    #print("x: ", x)
-    #print("y: ", y)
-
-#p1 = win.addPlot(title="Basic array plotting", y=np.random.normal(size=100))
-p1 = win.addPlot(title="testing")
-#p1.setDownsampling(mode='peak')
+# set graph parameters
+p1 = win.addPlot(title="testing")           # title
+#p1.setDownsampling(mode='peak')             # reduce drawing load
 #p1.setClipToView(True)
-p1.showGrid(x=True, y=True)
-p1.setRange(xRange=[-axisRange,axisRange], yRange=[-axisRange,axisRange])
-p1.plot(dataX, dataY)
-p1.plot(circleX, circleY)
+p1.showGrid(x=True, y=True)                 # print grid
+p1.setRange(xRange=[-axisRange,axisRange],  # set axis range
+            yRange=[-axisRange,axisRange])
+
+#don't know what this does yet, used for "live" drawing
+#curve = p1.plot()
+
+angle = startAngle
+# print stationary circle
+for t in range(0,oneRotation * 10):
+    x = statRad * cos(angle)
+    y = statRad * sin(angle)
+
+    statRadX[t] = x
+    statRadY[t] = y
+
+    print("x: ",x,", y: ",y)
+
+    angle += theta
+
+# draw stationary circle
+p1.plot(statRadX, statRadY)
+
+angle = startAngle
+#for t in range(0,steps):
+
+#    # hypotrochoid
+#    # add X and Y values to corresponding arrays
+#    rotRadX[t] = (statRad - rotRad) * cos(angle) + drawPointRad * cos(((statRad - rotRad) / rotRad) * angle)
+#    rotRadY[t] = (statRad - rotRad) * sin(angle) - drawPointRad * sin(((statRad - rotRad) / rotRad) * angle)
+
+#    # draw curve
+#    curve.setData(rotRadX[:t], rotRadY[:t])
+
+#    # move calculation point clockwise theta
+#    angle+=theta    
+
+#    sleep(0.05) # TEST
+
+# print plot after calculating all the points
+# p1.plot(rotRadX, rotRadY)
+
+
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     import sys
