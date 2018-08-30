3e24967789b3ad74c45580ee5d79d38e70707dae
dftbat2sp <pulse@tuta.io>
2018-08-29 22:06:12 -0400

    started drawing

--- trochoids_2/qtGraphTrochoids.py
+++ trochoids_2/qtGraphTrochoids.py
@@ -11,14 +11,15 @@ win.resize(1000,1000)               # set window size
 
 # Circle sizes
 statRad = 144               # Stationary Circle size
-rotRad = 72                 # moving circle size
-drawPointRad = rotRad/2     # location from center of moving circle to draw
+rotRad = 74                 # moving circle size
+drawPointRad = rotRad / 2     # location from center of moving circle to draw
 
 # Drawing paarameters
 startAngle = 0                       # starting angle
 theta = 0.05                    # distance in radians for each step
-oneRotation = int(3.14/theta)        # number of steps for 1 full rotation
-# should only need to do number of rotation less than the ratius of the stationary circle
+oneRotation = int(3.14 / theta)        # number of steps for 1 full rotation
+# should only need to do number of rotation less than the ratius of the
+                               # stationary circle
 steps = int(statRad * oneRotation) 
 
 axisRange = statRad + 50   # Axis label range
@@ -33,7 +34,7 @@ rotRadY = np.empty(steps) # corresponding y points
 
 # set graph parameters
 p1 = win.addPlot(title="testing")           # title
-#p1.setDownsampling(mode='peak')             # reduce drawing load
+p1.setDownsampling(mode='peak') # reduce drawing load
 #p1.setClipToView(True)
 p1.showGrid(x=True, y=True)                 # print grid
 p1.setRange(xRange=[-axisRange,axisRange],  # set axis range
@@ -41,13 +42,12 @@ p1.setRange(xRange=[-axisRange,axisRange],  # set axis range
 
 #don't know what this does yet, used for "live" drawing
 #curve = p1.plot()
-
 angle = startAngle
 # print stationary circle
-for t in range(0,oneRotation * 10):
+for t in range(0,130):
     x = statRad * cos(angle)
     y = statRad * sin(angle)
-
+    7
     statRadX[t] = x
     statRadY[t] = y
 
@@ -58,25 +58,46 @@ for t in range(0,oneRotation * 10):
 # draw stationary circle
 p1.plot(statRadX, statRadY)
 
+# reset starting angle
 angle = startAngle
+
+# print fancy things
+curve = p1.plot()
 #for t in range(0,steps):
 
-#    # hypotrochoid
-#    # add X and Y values to corresponding arrays
-#    rotRadX[t] = (statRad - rotRad) * cos(angle) + drawPointRad * cos(((statRad - rotRad) / rotRad) * angle)
-#    rotRadY[t] = (statRad - rotRad) * sin(angle) - drawPointRad * sin(((statRad - rotRad) / rotRad) * angle)
+data3 = np.empty(100)
+ptr3 = 0
+t = 0
 
-#    # draw curve
-#    curve.setData(rotRadX[:t], rotRadY[:t])
+def update1():
+    global rotRadX, rotRadY, curve, angle, ptr3, t
+    # hypotrochoid
+    # add X and Y values to corresponding arrays
+    rotRadX[t] = (statRad - rotRad) * cos(angle) + drawPointRad * cos(((statRad - rotRad) / rotRad) * angle)
+    rotRadY[t] = (statRad - rotRad) * sin(angle) - drawPointRad * sin(((statRad - rotRad) / rotRad) * angle)
 
-#    # move calculation point clockwise theta
-#    angle+=theta    
+    # draw curve
+    #curve.setData(x=rotRadX[:t], y=rotRadY[:t])
+    curve.setData(rotRadX[:t],rotRadY[:t])
+    # move calculation point clockwise theta
+    angle+=theta  
+    t += 1
+#      
 
-#    sleep(0.05) # TEST
+
+
+def update():
+    update1()
+#sleep(0.05) # TEST
 
 # print plot after calculating all the points
 # p1.plot(rotRadX, rotRadY)
 
+timer = pg.QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(5)
+
+print ("done")
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
